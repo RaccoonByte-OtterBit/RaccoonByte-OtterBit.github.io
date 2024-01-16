af8f3815ae7ecfaed8bfe1abde352f390498badf
uoohoon <hoonixox@naver.com>
2024-01-16 21:05:47 +0900

    feat: add home, categroy section to home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-wrapper">
+      <header className="page-header">
+        <div className="page-header-wrapper">
+          <div className="home-section">
+            <a className="link" href="/">
+              bono-log
+            </a>
+          </div>
+          <div className="category-section">
+            <a className="link" href="/">
+              about
+            </a>
+            <a className="link" href="/">
+              pages
+            </a>
+          </div>
+        </div>
+      </header>
       <p>Hello, Bono-log!</p>
     </div>
   );

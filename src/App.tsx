b54c7884a56d80e67fb94e10a6c77e5f2deba797
otter <144116866+otterbits@users.noreply.github.com>
2024-01-24 23:35:47 +0900

    ✨Feature/9 home screen (#11)
    ## summary
    
    웹페이지에 react-markdown을 통하여 markdown 문서를 import하여 page-content 부분에
    출력하였습니다.
    
    - Resolves: #9 
    
    ## PR 유형
    
    어떤 유형인가요?
    
    **PR 제목 작성시 '[유형에 맞는 gitmoji]유형/ 제목'으로 입력 부탁드립니다.**
    
    - [x] ✨ Feature / 새 기능
    - [ ] 🐛 Fix / 버그 수정
    - [ ] 💄 Style / CSS 등 사용자 UI 디자인 변경
    - [ ] ♻️ Refactor / 코드 리팩토링
    - [ ] 📝 Docs / 문서 수정
    - [ ] ✅ Test / 테스트 추가, 테스트 리팩토링
    - [ ] 👷 CI / 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 🚚 Rename / 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경, 파일 및 폴더 이름
    변경)
    - [ ] 🔥 Remove / 파일 혹은 폴더 삭제
    
    ## 변경 사항
    
    - react-markdown 라이브러리를 설치하였습니다.
      - markdown을 github 블로그의 post 문서로 쓸 것이기 때문에 react 문법으로 변경해주는 과정이 필요합니다.
    <img width="641" alt="스크린샷 2024-01-24 오후 9 01 53"
    src="https://github.com/bono-log/bono-log.github.io/assets/144116866/e11821e0-c99e-4770-a9c4-8fc57f47085b">
    
    - markdown 문서를 public 폴더에 저장하였습니다.
      - public 폴더는 정적 파일을 보관하기 때문에 서버에 요청하지 않고도 미리 저장해놓을 수 있기 때문입니다. 
    - github blog는 배포를 해서 출력해주는 것이기 때문에 react 단독으로는 서버가 없다면 마크다운을 불러올 수 있는
    방법이 없다고 생각합니다.
    
    <img width="1018" alt="스크린샷 2024-01-24 오후 9 00 03"
    src="https://github.com/bono-log/bono-log.github.io/assets/144116866/2e199c28-5816-46f7-b0ba-5a4c79b2605e">
    
    - markdown을 App.tsx에 fetch로 불러와 출력하였습니다.
      - public 폴더 안에 markdown 문서가 있기 때문에 public 내의 파일 경로만 입력해줘도 됩니다.
    
    ## 참고 자료
    
    -
    [react-markdown](https://stackoverflow.com/questions/42928530/how-do-i-load-a-markdown-file-into-a-react-component)
    첫번째 답변을 참고하였습니다.
    
    
    ## PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 정상 동작 확인 여부
    - [ ] commit message convention 충족 여부

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [postContent, setPostContent] = useState('');
+
+  useEffect(() => {
+    const postPath = '/post/test.md';
+
+    fetch(postPath)
+      .then((response) => response.text())
+      .then((data) => {
+        const withoutFM = data.replace(/^---[\s\S]*?---/, ''); // Frontmatter 제거
+        setPostContent(withoutFM);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <p>Hello, Bono-log!</p>
+    <div className="page-wrapper">
+      <header className="page-header">
+        <div className="page-header-wrapper">
+          <div className="home-section">
+            <a className="link" href="/">
+              bono-log
+            </a>
+          </div>
+          <div className="category-section">
+            <a className="link" href="/">
+              about
+            </a>
+            <a className="link" href="/">
+              pages
+            </a>
+          </div>
+        </div>
+      </header>
+      <body>
+        <div className="page-content">
+          <p>Hello, Bono-log!</p>
+          <div className="post-card-wrapper">
+            <a className="post-card" href="/">
+              <div className="post-title">title</div>
+              <p className="post-content">
+                <ReactMarkdown>{postContent}</ReactMarkdown>
+              </p>
+            </a>
+          </div>
+        </div>
+      </body>
+      <footer className="page-footer-wrapper">
+        <div className="page-footer">
+          <p>copyright</p>
+        </div>
+      </footer>
     </div>
   );
 }

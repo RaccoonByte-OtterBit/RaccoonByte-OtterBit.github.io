54d4ed404ce9f24ecc9302c1d42fec57d6c3e03e
otter <144116866+otterbits@users.noreply.github.com>
2024-02-07 00:11:31 +0900

    ✨ post card ui 내용 요약 기능 추가 (#65)
    ## summary
    
    post card ui 안에 있는 내용을 요약해서 출력해주었습니다.
    
    - Resolves: #54 
    
    ## PR 유형
    
    어떤 유형인가요? 해당되는 유형에 체크해주세요.
    
    **PR 제목 작성시 '[유형에 맞는 gitmoji] 제목'으로 입력 부탁드립니다.**
    
    - [x] ✨ Feature / 새로운 기능
    - [ ] 🐛 Fix / 버그 수정
    - [ ] 💄 Style / CSS 등 사용자 UI 디자인 변경
    - [ ] ♻️ Refactor / 코드 리팩토링(구조 개선, 디렉토리 변경 등...)
    - [ ] 📝 Docs / 문서 수정
    - [ ] ✅ Test / 테스트 추가, 테스트 리팩토링
    - [ ] 💬 Chore / 그 외, 모든 변경점(Package, Rename, Remove, etc...)
    
    ## 변경 사항
    
    ### As-is
    
    ![image](https://github.com/bono-log/bono-log.github.io/assets/144116866/586c184e-763e-4c6a-b5dd-05740e0bdc42)
    
    ### To-be
    
    ![image](https://github.com/bono-log/bono-log.github.io/assets/144116866/11c4e6f1-0d20-485e-a5b4-cee93ae8d782)
    
    - post card 내용이 최대 3줄까지 출력되고, 나머지 문자는 '...'로 표시되도록 하였습니다.
    - markdown 문법을 제거하여 출력하도록 하였습니다.(제목이 다르거나, '#', '##'과 같은 것이 그대로 출력되면
    가독성, 미관상에 좋지 않습니다.)
    - raccoon의 md에 줄바꿈 문법을 정규식이 아닌 마크다운 문법으로 바꾸어주었습니다.
      - '\' -> '  '(더블 스페이스)
    
    
    
    ## PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 정상 동작 확인 여부
    - [x] commit message convention 충족 여부

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
 import { Link } from 'react-router-dom';
 
 interface PostData {
@@ -13,7 +12,17 @@ interface PostData {
 interface PostCardProps {
   postList: PostData[];
 }
+function extractMarkdown(markdownText: string) {
+  const content = markdownText
+    .replace(/^(#+)\s+(.*)/gm, '$2') // 제목 헤더 제거
+    .replace(/^[-*]\s+(.*)/gm, '$1') // 목록 항목 제거
+    .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1') // 링크 제거
+    .replace(/!\[([^\]]+)\]\([^)]+\)/g, '') // 이미지 제거
+    .replace(/^\s*>.*$/gm, '') // 인용문 제거
+    .replace(/\n{2,}/g, '\n'); // 여러 줄바꿈을 하나로 줄임
 
+  return content.trim(); // 앞뒤 공백 제거
+}
 function PostCard({ postList }: PostCardProps) {
   return (
     <div className="post-card-list">
@@ -25,9 +34,7 @@ function PostCard({ postList }: PostCardProps) {
             className="post-card"
           >
             <div className="post-title">{postData.title}</div>
-            <p className="post-content">
-              <ReactMarkdown>{postData.content}</ReactMarkdown>
-            </p>
+            <p className="post-content">{extractMarkdown(postData.content)}</p>
             <div className="post-info">
               <div className="post-date">{postData.date}</div>
               <div className="post-categories">{postData.categories}</div>

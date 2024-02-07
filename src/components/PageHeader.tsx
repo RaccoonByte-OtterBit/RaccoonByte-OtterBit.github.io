48300ab88a0b65ec72943d15aac9765bf35b4865
uoohoon <hoonixox@naver.com>
2024-02-07 21:08:40 +0900

    refactor: add useLocation in PageHeader.tsx font-weight로 각 url로 접속 시 접속했다고 표시

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 function PageHeader() {
+  const currentPage = useLocation().pathname;
+
   return (
     <div className="page-header-wrapper">
       <header className="page-header">
         <div className="home-section">
-          <a className="link" href="/">
+          <a className={`link ${currentPage === '/' ? 'active' : ''}`} href="/">
             bono-log
           </a>
         </div>

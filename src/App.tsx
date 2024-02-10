8bef7cff9d01f6435c711654db29ff5f80b438e4
uoohoon <hoonixox@naver.com>
2024-02-10 21:17:09 +0900

    refactor: unify router path 'about' and 'detail'

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="post/:id" element={<PageDetail />} />
+          <Route path="/post/:id" element={<PageDetail />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <PageFooter />
